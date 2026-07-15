cb5de6ccdc3be3708bde1217b05730608ba75559
Karimskee <kareemgamer2006@gmail.com>
2026-07-15 03:15:11 +0300

    fix: vercel deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         unoptimized: true
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/fcai-usc-courses-tracker' : '',
+    }
 };
 
 export default nextConfig;
